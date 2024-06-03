3964777f835eefe2cc17488ca9925888a581dfbd
HloleloRampete <156667505+HloleloRampete@users.noreply.github.com>
2024-06-03 13:13:43 +0200

    "Set Up the React Application and Create the Home Components"

---
+++ src/components/Home.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+        <div className="home">
+            <h1>Welcome to VanLife</h1>
+            <p>Your adventure begins here!</p>
+        </div>
+    )
+}
