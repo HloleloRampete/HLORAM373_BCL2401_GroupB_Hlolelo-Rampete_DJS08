edd903a252be6ab6109431f98711f9a67c79a0e2
HloleloRampete <156667505+HloleloRampete@users.noreply.github.com>
2024-06-03 13:54:52 +0200

    "Update Home component with new content and add Link to 'vans' route"

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom"
 
 export default function Home() {
   return (
-    <div className="home">
-      <h1>Welcome to VanLife</h1>
-      <p>Your adventure begins here!</p>
-    </div>
+<div className="home-container">
+            <h1>You got the travel plans, we got the travel vans.</h1>
+            <p>Add adventure to your life by joining the #vanlife movement. Rent the perfect van to make your perfect road trip.</p>
+            <Link to="vans">Find your van</Link>
+        </div>
   );
 }
