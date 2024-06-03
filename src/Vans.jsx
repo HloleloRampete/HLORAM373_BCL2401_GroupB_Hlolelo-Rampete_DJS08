63d8e78944cb212703ee4e8f3a6bfd23ee99a01a
HloleloRampete <156667505+HloleloRampete@users.noreply.github.com>
2024-06-03 16:02:02 +0200

    "Added state and effect to fetch vans data, and created van elements from the data"

--- src/Vans.jsx
+++ src/Vans.jsx
@@ -1,12 +1,25 @@
 import React from "react";
 
 export default function Vans() {
-    
+  const [vans, setVans] = React.useState([]);
+
   React.useEffect(() => {
     fetch("/api/vans")
       .then((response) => response.json())
-      .then((data) => console.log(data));
+      .then((data) => setVans(data.vans));
   }, []); // Avoiding our effect from running everytime our page updates.
-  
+  // We only want it to run once, when our component mounts.
+
+const vanElements = vans.map(van => (
+    <div key={van.id} className="van-title">
+        <img src={van.imageUrl} alt="Van image" />
+        <div className="van-info">
+            <h3>{van.name}</h3>
+            <p>${van.price}<span>/day</span></p>
+        </div>
+        <i className={`van-type ${van.type} selected`}>{van.type}</i>
+    </div>
+))
+
   return <h1></h1>;
 }
