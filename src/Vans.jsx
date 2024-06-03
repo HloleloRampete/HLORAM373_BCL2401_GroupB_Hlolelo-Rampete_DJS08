8e43bb40922e08a94139bcef3f9f43029b9e6649
HloleloRampete <156667505+HloleloRampete@users.noreply.github.com>
2024-06-03 15:52:49 +0200

    "Added React.useEffect hook to fetch data from /api/vans in Vans component"

--- src/Vans.jsx
+++ src/Vans.jsx
@@ -1,7 +1,12 @@
-import React from "react"
+import React from "react";
 
 export default function Vans() {
-    return (
-        <h1></h1>
-    )
+    
+  React.useEffect(() => {
+    fetch("/api/vans")
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  }, []); // Avoiding our effect from running everytime our page updates.
+  
+  return <h1></h1>;
 }
