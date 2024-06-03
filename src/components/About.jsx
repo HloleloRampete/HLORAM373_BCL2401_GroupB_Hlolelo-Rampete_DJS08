1109653b4be3d020937c8f9764688c75bdc1b9c3
HloleloRampete <156667505+HloleloRampete@users.noreply.github.com>
2024-06-03 13:16:42 +0200

    Created About component

---
+++ src/components/About.jsx
@@ -0,0 +1,8 @@
+export default function About() {
+    return (
+        <div className="about">
+            <h1>About VanLife</h1>
+            <p>Learn more about out mission and values.</p>
+        </div>
+    )
+}
