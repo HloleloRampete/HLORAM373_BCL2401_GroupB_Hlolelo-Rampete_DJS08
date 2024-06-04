506575b3137c7f149bb67e325586d36409c28d90
HloleloRampete <156667505+HloleloRampete@users.noreply.github.com>
2024-06-04 15:06:22 +0200

    "Refactor App.jsx to add new routes and components, update Header.jsx to include new link, and remove VanDetail.jsx and Vans.jsx files"

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,11 @@ import Home from "./pages/Home.jsx";
 import About from "./pages/About.jsx";
 // import Navbar from "./components/Navbar.jsx";
 import "./server.js";
-import Vans from "./pages/Vans.jsx";
-import VanDetail from "./pages/VanDetail.jsx";
+import Vans from "./pages/Vans/Vans.jsx";
+import VanDetail from "./pages/Vans/VanDetail.jsx";
+import Dashboard from "./pages/Host/Dashboard.jsx"
+import Income from "./pages/Host/Income.jsx"
+import Reviews from "./pages/Host/Reviews.jsx"
 import Layout from "./components/Layout.jsx"
 
 export default function App() {
@@ -17,6 +20,9 @@ export default function App() {
           <Route path="/about" element={<About />} />
           <Route path="/vans" element={<Vans />} />
           <Route path="/vans/:id" element={<VanDetail />} />
+          <Route path="/host" element={<Dashboard />} />
+          <Route path="/host/income" element={<Income />} />
+          <Route path="/host/reviews" element={<Reviews />} />
         </Route>
       </Routes>
     </BrowserRouter>
