306ec835fb4ea5e0caf497d4c8b62174c435f57e
HloleloRampete <156667505+HloleloRampete@users.noreply.github.com>
2024-06-04 11:23:56 +0200

    "Removed server.js file and updated VanDetail component to fetch data from API using react-router-dom"

--- src/components/VanDetail.jsx
+++ src/components/VanDetail.jsx
@@ -1,7 +1,28 @@
 import React from "react"
+import { useParams } from "react-router-dom"
 
 export default function VanDetail() {
+    const params = useParams()
+    const [van, setVan] = React.useState(null)
+
+    React.useEffect(() => {
+        // fetch data from API using params.id
+        fetch(`/api/vans/${params.id}`)
+        .then(response => response.json())
+        .then(data => setVan(data.vans))
+    }, [params.id]) // Rerunning the useEffect for each time data is fetched
+
     return (
-        <h1>Van details</h1>
+        <div className="van-detail-container">
+            {van ? (
+                <div className="van-detail">
+                    <img src={van.imageUrl} alt="Van image" />
+                    <h2>{van.name}</h2>
+                    <p className="van-price"><span>${van.price}</span>/day</p>
+                    <p>{van.description}</p>
+                    <button className="link-button">Rent this van</button>
+                </div>
+            ) : <h2>Loading...</h2>}
+        </div>
     )
 }
