8fa1c8ed535ca88a273b2e262b2a00da8c218c76
HloleloRampete <156667505+HloleloRampete@users.noreply.github.com>
2024-06-04 15:38:04 +0200

    "Updated App.jsx to fix import statements and route definitions; added content to Income.jsx"

--- src/App.jsx
+++ src/App.jsx
@@ -6,23 +6,24 @@ import About from "./pages/About.jsx";
 import "./server.js";
 import Vans from "./pages/Vans/Vans.jsx";
 import VanDetail from "./pages/Vans/VanDetail.jsx";
-import Dashboard from "./pages/Host/Dashboard.jsx"
-import Income from "./pages/Host/Income.jsx"
-import Reviews from "./pages/Host/Reviews.jsx"
-import Layout from "./components/Layout.jsx"
+import Dashboard from "./pages/Host/Dashboard.jsx";
+import Income from "./pages/Host/Income.jsx";
+import Reviews from "./pages/Host/Reviews.jsx";
+import Layout from "./components/Layout.jsx";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route element={<Layout />} >
+        <Route element={<Layout />}>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/vans" element={<Vans />} />
           <Route path="/vans/:id" element={<VanDetail />} />
-          <Route path="/host" element={<Dashboard />} />
-          <Route path="/host/income" element={<Income />} />
-          <Route path="/host/reviews" element={<Reviews />} />
+          <Route path="/host" element={<Dashboard />}>
+            <Route path="/host/income" element={<Income />} />
+            <Route path="/host/reviews" element={<Reviews />} />
+          </Route>
         </Route>
       </Routes>
     </BrowserRouter>
