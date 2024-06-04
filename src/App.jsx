208777ab177e958434b9b0ebac75dbf95225093f
HloleloRampete <156667505+HloleloRampete@users.noreply.github.com>
2024-06-04 13:39:21 +0200

    "Removed Link import and navbar code from App component and put it under the newly created Header component"

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 // import React from "react"
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route} from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import About from "./pages/About.jsx";
 // import Navbar from "./components/Navbar.jsx";
@@ -7,34 +7,11 @@ import "./server.js";
 import Vans from "./pages/Vans.jsx";
 import VanDetail from "./pages/VanDetail.jsx"
 
-/**
- * Challenge:
- * Bootstrap the VanLife project by creating the first 2 routes:
- * Home and About.
- *
- * Also include the navbar that can link between the two routes.
- * For now, you'll either need to copy/paste the navbar code
- * to both Home and About pages, or you'll need to find a place
- * to put it where it can be shared between the two pages.
- * (Don't overthink this part - just do whatever is easiest for
- * you because we'll learn a better approach very soon)
- *
- * Review challenge: do all the CSS yourself based on the design
- * linked in the slides.
- */
+
 
 export default function App() {
   return (
     <BrowserRouter>
-      <header>
-        <Link className="site-logo" to="/">
-          #VANLIFE
-        </Link>
-        <nav>
-          <Link to="/about">About</Link>
-          <Link to="/vans">Vans</Link>
-        </nav>
-      </header>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
