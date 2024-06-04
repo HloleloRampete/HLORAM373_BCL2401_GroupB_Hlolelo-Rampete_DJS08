e70dc3f399f6cb03c1cccc0b4f9a6a84dc6551fd
HloleloRampete <156667505+HloleloRampete@users.noreply.github.com>
2024-06-04 14:43:44 +0200

    "Added Layout component and wrapped routes with it in App.jsx"

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,23 @@
 // import React from "react"
-import { BrowserRouter, Routes, Route} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import About from "./pages/About.jsx";
 // import Navbar from "./components/Navbar.jsx";
 import "./server.js";
 import Vans from "./pages/Vans.jsx";
-import VanDetail from "./pages/VanDetail.jsx"
-
-
+import VanDetail from "./pages/VanDetail.jsx";
+import Layout from "./components/Layout.jsx"
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/vans" element={<Vans />} />
-         <Route path="/vans/:id" element={<VanDetail />} /> 
+        <Route element={<Layout />} >
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/vans" element={<Vans />} />
+          <Route path="/vans/:id" element={<VanDetail />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
