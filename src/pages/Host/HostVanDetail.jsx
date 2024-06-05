6c6718d2e618a92d9ac3919d8df8532ff7df3617
HloleloRampete <156667505+HloleloRampete@users.noreply.github.com>
2024-06-05 16:13:09 +0200

    "Updated HostVanDetail component: added semicolons, reformatted code, and replaced placeholder return statement with actual van detail layout"

--- src/pages/Host/HostVanDetail.jsx
+++ src/pages/Host/HostVanDetail.jsx
@@ -1,17 +1,30 @@
-import React from "react"
-import { useParams } from "react-router-dom"
+import React from "react";
+import { useParams } from "react-router-dom";
 
 export default function HostVanDetail() {
-    const { id } = useParams()
-    const [currentVan, setCurrentVan] = React.useState(null)
+  const { id } = useParams();
+  const [currentVan, setCurrentVan] = React.useState(null);
 
-    React.useEffect(() => {
-        fetch(`/api/host/vans/${id}`)
-        .then(response => response.json())
-        .then(data => setCurrentVan(data.vans))
-    }, [])
+  React.useEffect(() => {
+    fetch(`/api/host/vans/${id}`)
+      .then((response) => response.json())
+      .then((data) => setCurrentVan(data.vans));
+  }, []);
 
-    return (
-        <h1>Hi</h1>
-    )
+  return (
+    <section>
+      <div className="host-van-detail-layout-container">
+        <div className="host-van-detail">
+          <img src={currentVan.imageUrl} />
+          <div className="host-van-detail-info-text">
+            <i className={`van-type van-type-${currentVan.type}`}>
+              {currentVan.type}
+            </i>
+            <h3>{currentVan.name}</h3>
+            <h4>${currentVan.price}/day</h4>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
