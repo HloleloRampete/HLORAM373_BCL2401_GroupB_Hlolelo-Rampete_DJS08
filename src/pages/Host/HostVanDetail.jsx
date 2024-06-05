e41062ca5ff5a5bce49b2da4f835ad27596637b1
HloleloRampete <156667505+HloleloRampete@users.noreply.github.com>
2024-06-05 16:11:58 +0200

    "Added useParams and useState to HostVanDetail component, implemented useEffect to fetch data from API"

--- src/pages/Host/HostVanDetail.jsx
+++ src/pages/Host/HostVanDetail.jsx
@@ -1,6 +1,16 @@
 import React from "react"
+import { useParams } from "react-router-dom"
 
 export default function HostVanDetail() {
+    const { id } = useParams()
+    const [currentVan, setCurrentVan] = React.useState(null)
+
+    React.useEffect(() => {
+        fetch(`/api/host/vans/${id}`)
+        .then(response => response.json())
+        .then(data => setCurrentVan(data.vans))
+    }, [])
+
     return (
         <h1>Hi</h1>
     )
