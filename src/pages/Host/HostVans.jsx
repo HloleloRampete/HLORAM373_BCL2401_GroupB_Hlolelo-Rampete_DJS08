c2ef46fee6eae0027025f6d92abad28282a47531
HloleloRampete <156667505+HloleloRampete@users.noreply.github.com>
2024-06-05 15:53:51 +0200

    "Added state, effect, and JSX elements to HostVans component"

--- src/pages/Host/HostVans.jsx
+++ src/pages/Host/HostVans.jsx
@@ -1,7 +1,30 @@
-import React from "react"
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default function HostVans() {
-    return (
-        <h1>Hi</h1>
-    )
+  const [vans, setVans] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch("/api/host/vans")
+      .then((response) => response.json())
+      .then((data) => setVans(data.vans));
+  }, []);
+
+  const hostVansEls = vans.map((van) => (
+    <Link
+      to={`/host/vans/${van.id}`}
+      key={van.id}
+      className="host-van-link-wrapper"
+    >
+      <div className="host-van-single" key={van.id}>
+        <img src={van.imageUrl} alt={`Photo of ${van.name}`} />
+        <div className="host-van-info">
+          <h3>{van.name}</h3>
+          <p>${van.price}/day</p>
+        </div>
+      </div>
+    </Link>
+  ));
+
+  return <h1>Hi</h1>;
 }
