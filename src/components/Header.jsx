42bbd935c3f4df7ee88d3bdc2e6b613c0cf17a06
HloleloRampete <156667505+HloleloRampete@users.noreply.github.com>
2024-06-05 14:26:27 +0200

    "Updated Footer and Header components with minor syntax changes"

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,19 +10,19 @@ export default function Header() {
       <nav>
         <NavLink
           to="/host"
-          className={({ isActive }) => (isActive ? "active-link" : null)}
+          className={({ isActive }) => isActive ? "active-link" : null}
         >
           Host
         </NavLink>
         <NavLink
           to="/about"
-          className={({ isActive }) => (isActive ? "active-link" : null)}
+          className={({ isActive }) => isActive ? "active-link" : null}
         >
           About
         </NavLink>
         <NavLink
           to="/vans"
-          className={({ isActive }) => (isActive ? "active-link" : null)}
+          className={({ isActive }) => isActive ? "active-link" : null}
         >
           Vans
         </NavLink>
