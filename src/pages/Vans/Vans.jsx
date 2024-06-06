496fc5ab2c4ea56935b59747cf500427b79a1dc5
HloleloRampete <156667505+HloleloRampete@users.noreply.github.com>
2024-06-06 16:26:26 +0200

    "Added van list filter buttons to Vans page"

--- src/pages/Vans/Vans.jsx
+++ src/pages/Vans/Vans.jsx
@@ -37,6 +37,32 @@ export default function Vans() {
   return (
     <div className="van-list-container">
       <h1>Explore our van options</h1>
+      <div className="van-list-filter-buttons">
+        <Link
+          to="?type=simple"
+          className="van-type simple"
+        >
+          Simple
+        </Link>
+        <Link
+          to="?type=Luxury"
+          className="van-type luxury"
+        >
+          Luxury
+        </Link>
+        <Link
+          to="?type=Rugged"
+          className="van-type rugged"
+        >
+          Rugged
+        </Link>
+        <Link
+          to="."
+          className="van-type clear-filters"
+        >
+          Clear filters
+        </Link>
+      </div>
       <div className="van-list">{vanElements}</div>
     </div>
   );
