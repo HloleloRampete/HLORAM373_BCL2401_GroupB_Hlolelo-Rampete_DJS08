30e6fd5df1953d873a1117e2c0d41a8355eebb28
HloleloRampete <156667505+HloleloRampete@users.noreply.github.com>
2024-06-06 14:01:54 +0200

    "Added new routes and components for HostVanDetail page, including HostVanInfo, HostVanPricing, and HostVanPhotos"

--- src/App.jsx
+++ src/App.jsx
@@ -9,27 +9,39 @@ import VanDetail from "./pages/Vans/VanDetail.jsx";
 import Dashboard from "./pages/Host/Dashboard.jsx";
 import Income from "./pages/Host/Income.jsx";
 import Reviews from "./pages/Host/Reviews.jsx";
-import HostVans from "./pages/Host/HostVans.jsx"
-import HostVanDetail from "./pages/Host/HostVanDetail.jsx"
+import HostVans from "./pages/Host/HostVans.jsx";
+import HostVanDetail from "./pages/Host/HostVanDetail.jsx";
+import HostVanInfo from "./pages/Host/HostVanInfo.jsx";
+import HostVanPricing from "./pages/Host/HostVanPricing.jsx";
+import HostVanPhotos from "./pages/Host/HostVanPhotos.jsx";
 import Layout from "./components/Layout.jsx";
 import HostLayout from "./components/HostLayout.jsx";
-
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
+          {" "}
+          {/* turned into a layout route */}
           <Route index element={<Home />} />
           <Route path="about" element={<About />} />
           <Route path="vans" element={<Vans />} />
           <Route path="vans/:id" element={<VanDetail />} />
           <Route path="host" element={<HostLayout />}>
+            {" "}
+            {/* turned into a layout route */}
             <Route index element={<Dashboard />} />
             <Route path="income" element={<Income />} />
             <Route path="reviews" element={<Reviews />} />
             <Route path="vans" element={<HostVans />} />
-            <Route path="vans/:id" element={<HostVanDetail />} />
+            <Route path="vans/:id" element={<HostVanDetail />}>
+              {" "}
+              {/* turned into a layout route */}
+              <Route index element={<HostVanInfo />} />
+              <Route path="pricing" element={<HostVanPricing />} />
+              <Route path="photos" element={<HostVanPhotos />} />
+            </Route>
           </Route>
         </Route>
       </Routes>
