799c3eb56b3d64e643efd76617ad120a809ea2c1
HloleloRampete <156667505+HloleloRampete@users.noreply.github.com>
2024-06-09 11:11:38 +0200

    "Updated VanDetail component: added conditional rendering for van data, removed loading indicator, and added van type icon"

--- src/pages/Vans/VanDetail.jsx
+++ src/pages/Vans/VanDetail.jsx
@@ -41,9 +41,11 @@ export default function VanDetail() {
       <Link to={`..${search}`} relative="path" className="back-button">
         &larr; <span>Back to {type} vans</span>
       </Link>
-      {van ? (
+
+      {van && (
         <div className="van-detail">
-          <img src={van.imageUrl} alt="Van image" />
+          <img src={van.imageUrl} />
+          <i className={`van-type ${van.type} selected`}>{van.type}</i>
           <h2>{van.name}</h2>
           <p className="van-price">
             <span>${van.price}</span>/day
@@ -51,8 +53,6 @@ export default function VanDetail() {
           <p>{van.description}</p>
           <button className="link-button">Rent this van</button>
         </div>
-      ) : (
-        <h2>Loading...</h2>
       )}
     </div>
   );
