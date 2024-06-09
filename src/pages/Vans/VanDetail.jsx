549fbdb96725d13248ccd5f0b0c166807cf10237
HloleloRampete <156667505+HloleloRampete@users.noreply.github.com>
2024-06-09 11:10:57 +0200

    "Refactor VanDetail component to use getVan API function, add loading and error states, and handle API response"

--- src/pages/Vans/VanDetail.jsx
+++ src/pages/Vans/VanDetail.jsx
@@ -1,17 +1,37 @@
 import React from "react";
 import { Link, useParams, useLocation } from "react-router-dom";
+import { getVan } from "../../api";
 
 export default function VanDetail() {
-  const params = useParams();
-  const location = useLocation();
   const [van, setVan] = React.useState(null);
+  const [loading, setLoading] = React.useState(false);
+  const [error, setError] = React.useState(null);
+  const { id } = useParams();
+  const location = useLocation();
 
   React.useEffect(() => {
     // fetch data from API using params.id
-    fetch(`/api/vans/${params.id}`)
-      .then((response) => response.json())
-      .then((data) => setVan(data.vans)); // localized state
-  }, [params.id]); // Rerunning the useEffect for each time data is fetched
+    async function loadVans() {
+      setLoading(true);
+      try {
+        const data = await getVan(id);
+        setVan(data);
+      } catch (err) {
+        setError(err);
+      } finally {
+        setLoading(false);
+      }
+    }
+    loadVans();
+  }, [id]); // Rerunning the useEffect for each time data is fetched
+
+  if (loading) {
+    return <h1>Loading...</h1>;
+  }
+
+  if (error) {
+    return <h1>There was an error: {error.message}</h1>;
+  }
 
   const search = location.state?.search || "";
   const type = location.state?.type || "all";
