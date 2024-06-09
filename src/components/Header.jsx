25736863c465cd270398c74ee3fcf8ebd42bb12f
HloleloRampete <156667505+HloleloRampete@users.noreply.github.com>
2024-06-09 12:06:42 +0200

    "Updated Header component with new styles, added login link and logout button, and refactored NavLink active styles"

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,32 +1,48 @@
 import React from "react";
 import { Link, NavLink } from "react-router-dom";
+import imageUrl from "../../assets/images/avatar-icon.png";
 
 export default function Header() {
+  const activeStyles = {
+    fontWeight: "bold",
+    textDecoration: "underline",
+    color: "#161616",
+  };
+
+  function fakeLogOut() {
+    localStorage.removeItem("loggedin");
+  }
+
   return (
     <header>
       <Link className="site-logo" to="/">
-        #VANLIFE
+        #VanLife
       </Link>
       <nav>
         <NavLink
           to="/host"
-          className={({ isActive }) => isActive ? "active-link" : null} // render prop
+          style={({ isActive }) => (isActive ? activeStyles : null)}
         >
           Host
         </NavLink>
         <NavLink
           to="/about"
-          className={({ isActive }) => isActive ? "active-link" : null}
+          style={({ isActive }) => (isActive ? activeStyles : null)}
         >
           About
         </NavLink>
         <NavLink
           to="/vans"
-          className={({ isActive }) => isActive ? "active-link" : null}
+          style={({ isActive }) => (isActive ? activeStyles : null)}
         >
           Vans
         </NavLink>
+        <Link to="login" className="login-link">
+          <img src={imageUrl} className="login-icon" />
+        </Link>
+        <button onClick={fakeLogOut}>X</button>
       </nav>
     </header>
   );
 }
+
