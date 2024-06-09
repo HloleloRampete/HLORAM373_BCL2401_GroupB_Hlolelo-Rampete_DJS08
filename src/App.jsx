44227c8d4a2fc9cfb893cd6088ba421da41e4299
HloleloRampete <156667505+HloleloRampete@users.noreply.github.com>
2024-06-09 11:28:07 +0200

    "Added Login page and AuthRequired component, reorganized route structure"

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import About from "./pages/About.jsx";
+import Login from "./pages/Login";
 // import Navbar from "./components/Navbar.jsx";
 import "./server.js";
 import Vans from "./pages/Vans/Vans.jsx";
@@ -17,35 +18,33 @@ import HostVanPhotos from "./pages/Host/HostVanPhotos.jsx";
 import NotFound from "./pages/NotFound.jsx";
 import Layout from "./components/Layout.jsx";
 import HostLayout from "./components/HostLayout.jsx";
+import AuthRequired from "./components/AuthRequired.jsx";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Layout />}>
-          {" "}
-          {/* turned into a layout route */}
+      <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="about" element={<About />} />
           <Route path="vans" element={<Vans />} />
           <Route path="vans/:id" element={<VanDetail />} />
-          <Route path="host" element={<HostLayout />}>
-            {" "}
-            {/* turned into a layout route */}
-            <Route index element={<Dashboard />} />
-            <Route path="income" element={<Income />} />
-            <Route path="reviews" element={<Reviews />} />
-            <Route path="vans" element={<HostVans />} />
-            
-            <Route path="vans/:id" element={<HostVanDetail />}>
-              {" "}
-              {/* turned into a layout route */}
-              <Route index element={<HostVanInfo />} />
-              <Route path="pricing" element={<HostVanPricing />} />
-              <Route path="photos" element={<HostVanPhotos />} />
+          <Route path="login" element={<Login />} />
+
+          <Route element={<AuthRequired />}>
+            <Route path="host" element={<HostLayout />}>
+              <Route index element={<Dashboard />} />
+              <Route path="income" element={<Income />} />
+              <Route path="reviews" element={<Reviews />} />
+              <Route path="vans" element={<HostVans />} />
+              <Route path="vans/:id" element={<HostVanDetail />}>
+                <Route index element={<HostVanInfo />} />
+                <Route path="pricing" element={<HostVanPricing />} />
+                <Route path="photos" element={<HostVanPhotos />} />
+              </Route>
             </Route>
-          
           </Route>
+
           <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
